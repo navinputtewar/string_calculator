78e2f36fe9a24e4f3dcf06a9f5163915cafa0ca3
Navin Puttewar <newd31@gmail.com>
2024-05-11 13:20:31 +0530

    added logic for comma seperated string to have sum of values as per the failing test case

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,9 @@
 def string_calculator(input)
-  return 0 if input.empty?
-  input.to_i
+  return 0 if input.empty?  # Handle empty input
+  numbers = input.split(",")
+  if numbers.length == 1 
+    numbers.first.to_i # Handle single number input
+  else
+   numbers.map(&:to_i).sum # Handle comma seperated number input
+  end
 end
