3106af408d6cf21f15ab0b0f87c3c4edd8931de3
Navin Puttewar <newd31@gmail.com>
2024-05-11 13:07:51 +0530

    added test method to test single number in a string

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -8,4 +8,11 @@ class StringCalculatorTest < Minitest::Test
     result = string_calculator(input)
     assert_equal(expected_output, result)
   end
+
+  def test_single_number_returns_itself
+    input = "1"
+    expected_output = 1
+    result = string_calculator(input)
+    assert_equal(expected_output, result)
+  end
 end
