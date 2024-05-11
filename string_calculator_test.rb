b5bcb12567e29c4ed8f6583cc877c139761804db
Navin Puttewar <newd31@gmail.com>
2024-05-11 13:02:19 +0530

    added expected changes to test empty string

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -3,5 +3,9 @@ require './string_calculator'
 
 class StringCalculatorTest < Minitest::Test
   def test_empty_string_returns_zero
+    input = ""
+    expected_output = 0
+    result = string_calculator(input)
+    assert_equal(expected_output, result)
   end
 end
